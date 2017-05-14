89f5a98941afb32322db579214e7b997c466c0bc
15810856489 <www.baidu.com/p/>
2017-05-14 14:23:24 +0800

    ui

--- torando/first.py
+++ torando/first.py
@@ -3,15 +3,19 @@
    
 import tornado.ioloop
 import tornado.web
+import uimethods as mt
+import uimodle as md
    
    
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html',name="hahah")  # 渲染文件
+        self.render('index.html',name="hahah",npm="123")  # 渲染文件
 
 settings = {
     "template_path":"views", # 模板文件存放路径
-    "static_path":"static" # 静态文件存放路径
+    "static_path":"static", # 静态文件存放路径
+    "ui_methods":mt, #自定义方法
+    "ui_modules":md #自定义类
 }
 
 application = tornado.web.Application([
