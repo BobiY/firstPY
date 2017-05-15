c5e26e7034a45580886afc97dcf2f46f7f80ceb5
wb-ysb252760 <wb-ysb252760@alibaba-inc.com>
2017-05-15 14:02:02 +0800

    session finally

--- torando/first.py
+++ torando/first.py
@@ -9,6 +9,11 @@ from session import Session
 
 class BaseHandler(tornado.web.RequestHandler):
     def initialize(self):
+        # 防止重复创建随机字符串
+        if self.get_cookie("aa",None):
+            self.random_str = self.get_cookie("aa",None)
+        else:
+            self.random_str = ""
         self.session = Session(self)
 
    
@@ -16,16 +21,14 @@ class MainHandler(BaseHandler):
     def get(self):
         # print(str(self.get_secure_cookie("ok"),encoding='utf8')) #根据键值获取cookie,加密方式获得的cookie是字节类型，这里需要转字符串
         # self.set_secure_cookie('ok','aaaa') #设置cookie
-        self.session.set_value('name',"111111")
-        self.session.set_value('age',"bbbb")
-        self.session.set_value('is_login' , True)
+        self.session['name'] = "111111"
+        self.session['is_login'] = True
         self.set_cookie('aa',self.session.random_str)
         self.render('index.html',name="hahah",npm="123")  # 渲染文件
 
 class MangeHandler(BaseHandler):
     def get(self):
-        self.random_str = self.get_cookie("aa")
-        print(self.session.get_value("is_login"))
+        print(self.session["is_login"])
 
 settings = {
     "template_path": "views", # 模板文件存放路径
